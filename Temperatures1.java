2ee39301a59cdcdf31418ffeadd199a79c977cd7
nirav-a11y <niravkilledar2000@gmail.com>
2025-12-15 14:30:45 +0100

    Add files via upload

---
+++ Temperatures1.java
@@ -0,0 +1,104 @@
+import java.util.*; // Scanner, Locale
+import static java.lang.System.out;
+class Temperatures1
+{
+    public static void main (String[] args)
+    {
+        out.println("TEMPERATURES\n");
+// input tool
+        Scanner in = new Scanner(System.in);
+        in.useLocale(Locale.US);
+// enter the number of weeks and measurements
+        out.print("number of weeks: ");
+        int nofWeeks = in.nextInt();
+        out.print("number of measurements per week: ");
+        int nofMeasurementsPerWeek = in.nextInt();
+// storage space for temperature data
+        double[][] t = new double[nofWeeks + 1]
+                [nofMeasurementsPerWeek + 1];
+// read the temperatures
+        for (int week = 1; week <= nofWeeks; week++)
+        {
+            out.println("temperatures - week " + week + ":");
+            for (int measurement = 1;
+                 measurement <= nofMeasurementsPerWeek;
+                 measurement++)
+                t[week][measurement] = in.nextDouble();
+        }
+        out.println("");
+// show the temperatures
+        out.println("the temperatures");
+        for (int week = 1; week <= nofWeeks; week++)
+        {
+            for (int measurement = 1;
+                 measurement <= nofMeasurementsPerWeek;
+                 measurement++)
+                out.print(t[week][measurement] + " ");
+            out.println("");
+        }
+        out.println("");
+// the least, greatest and average temperatures - weekly
+        double[] minT = new double[nofWeeks + 1];
+        double[] maxT = new double[nofWeeks + 1];
+        double[] sumT = new double[nofWeeks + 1];
+        double[] avgT = new double[nofWeeks + 1];
+         // Assigning the first value of the respective weekly array to the variables
+        for(int week=1;week<=nofWeeks;week++) {
+            minT[week] = t[week][1];
+            maxT[week] = t[week][1];
+            sumT[week] = 0;
+            for (int measurement = 1; measurement <= nofMeasurementsPerWeek; measurement++)
+            {
+                if (maxT[week] < t[week][measurement])
+                {
+                    maxT[week] = t[week][measurement];
+                }
+                if (minT[week] > t[week][measurement])
+                {
+                    minT[week] = t[week][measurement];
+                }
+                sumT[week] = sumT[week] + t[week][measurement];
+                //accessing the current element and adding
+            }
+        avgT[week]= sumT[week]/nofMeasurementsPerWeek;
+        }
+// show the least, greatest and average temperatures
+        out.println("the least, greatest and average temperatures"
+                + " - weekly");
+        for (int week = 1; week <= nofWeeks; week++)
+            out.print(minT[week] + " ");
+        out.println("");
+        for (int week = 1; week <= nofWeeks; week++)
+            out.print(maxT[week] + " ");
+        out.println("");
+        for (int week = 1; week <= nofWeeks; week++)
+            out.print(avgT[week] + " ");
+        out.println("");
+        out.println();
+// the least, greatest and average temperatures - whole period
+        double minTemp = minT[1];
+        double maxTemp = maxT[1];
+        double sumTemp = sumT[1];
+        double avgTemp = 0;
+// add code here
+        for (int week = 2; week <= nofWeeks; week++)
+        {
+            if (minTemp > minT[week])
+                {
+                minTemp = minT[week];
+                }
+            if (maxTemp < maxT[week])
+            {
+                maxTemp = maxT[week];
+            }
+            sumTemp = sumTemp+sumT[week];
+        }
+        // now computing the average
+        avgTemp = sumTemp/(nofWeeks*nofMeasurementsPerWeek);
+// show the least, greatest and average temperature for
+// the whole period
+        out.println("the least, greatest and average temperature"
+                + " - whole period");
+        out.println(minTemp + "\n" + maxTemp + "\n" + avgTemp);
+    }
+}
